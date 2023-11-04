da9a6484af221a1c87aa06aab8c80d5aee68f3a1
Zamzami123 <89690519+Zamzami123@users.noreply.github.com>
2023-11-04 11:38:32 +0700

    settings.js

--- settings.js
+++ settings.js
@@ -1,10 +1,10 @@
 const fs = require('fs')
 
-global.creator = 'Zeeoneofc'// yourname
-global.MONGO_DB_URI = "" //database mongodb 
+global.creator = 'Sregep Shop'// yourname
+global.MONGO_DB_URI = "mongodb+srv://Zakizami1234:Zakizami1234@cluster0.ox7kbah.mongodb.net/?retryWrites=true&w=majority'" //database mongodb 
 global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
-global.your_email = "" //email
-global.email_password = "" //application password email
+global.your_email = "akunfrefirepertama@gmail.com" //email
+global.email_password = "bvec nakn dyrm tiks" //application password email
 global.limitCount = 10000
 global.YUOR_PORT = 8000
 global.loghandler = {
