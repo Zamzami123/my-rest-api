ea16b0e9f35e75f47ed16c663b3a0eb3a7f2fc8b
Zamzami123 <89690519+Zamzami123@users.noreply.github.com>
2023-11-04 13:26:17 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,7 +2,7 @@ const fs = require('fs')
 
 global.creator = 'SregepShop'// yourname
 global.MONGO_DB_URI = "mongodb+srv://Zakizami1234:Zakizami1234@cluster0.ox7kbah.mongodb.net/?retryWrites=true&w=majority" //database mongodb 
-global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
+global.ACTIVATION_TOKEN_SECRET = "SREGEP SHOP OFFICIAL" //isi apa aja bebas
 global.your_email = "akunfrefirepertama@gmail.com" //email
 global.email_password = "bvec nakn dyrm tiks" //application password email
 global.limitCount = 10000
